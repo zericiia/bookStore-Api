f417b21703b5c22b5b31bc818ac5b67d66415757
zericiia <karimbrahimi102@gmail.com>
2025-03-15 09:24:16 +0100

    testing cybersecurity

--- app.js
+++ app.js
@@ -1,43 +1,53 @@
 const express = require("express");
+const path = require("path");
 const mongoose = require("mongoose");
 const bookPath = require("./Routes/books");
 const authorsPath = require("./Routes/authors");
 const authPath = require("./Routes/auth");
-// middlewares;
 const logger = require("./middlewares/logger");
 const { notFound, errorHandler } = require("./middlewares/errorHandler");
-// 
 const dotenv = require("dotenv");
+
 dotenv.config();
-// conn to db
+
+// Connect to MongoDB
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
-    console.log(`connected to db succefully`);
+    console.log("Connected to DB successfully");
   })
   .catch((error) => {
-    console.log(`failed to connect to mongoDB  ${error}`);
+    console.log(`Failed to connect to MongoDB: ${error}`);
   });
 
-// init app
+// Initialize app
 const app = express();
-// json middleware
+
+// Serve static files from the "pages" directory
+app.use(express.static(path.resolve(__dirname, "pages")));
+
+// JSON middleware
 app.use(express.json());
 
-// custom middleware
+// Custom middleware
 app.use(logger);
 
-// ROUTES
+// Login route form
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "pages", "login.html"));
+});
+
+// Routes
 app.use("/api/books", bookPath);
 app.use("/api/authors", authorsPath);
 app.use("/api/auth", authPath);
-// error handler midleware
+
+// Error handler middleware
 app.use(notFound);
 app.use(errorHandler);
-// port
+
+// Start server
 const Port = process.env.PORT;
 app.listen(Port, () =>
-  console.log(
-    `server is running in ${process.env.NODE_ENV} mode on port ${Port}`
-  )
+  console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${Port}`)
 );
